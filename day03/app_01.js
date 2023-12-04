5795d003addaa4bc60a00279223c833c7362e307
cl3mcg <clementcg@outlook.com>
2023-12-05 00:49:16 +0100

    Adding answers

--- day03/app_01.js
+++ day03/app_01.js
@@ -7,3 +7,83 @@ const fileContent = fs.readFileSync('input.txt', 'utf8');
 // Create an array using the array.split() method
 // Split is performed using the carriage return as a separator
 const arrayOfLines = fileContent.split('\n');
+const numberOfRows = Number(arrayOfLines.length)
+const numberOfColumns = Number(arrayOfLines[0].trim().length)
+console.log(`Rows:`, numberOfRows, `Columns:`, numberOfColumns)
+
+// Parse the file to retrieve all the numbers
+const getAllNumbers = function () {
+    let linesData = [];
+    for (const line of arrayOfLines) {
+        let lineData = {
+            id: Number(arrayOfLines.indexOf(line) + 1),
+            numbers: []
+        };
+        let number = {
+            value: '',
+            validationCoordinates: []
+        };
+        for (let i = 0; i < numberOfColumns; i++) {
+            if (!isNaN(line[i])) {
+                number.value = `${number.value}${line[i]}`
+                number.validationCoordinates.push(
+                    { x: arrayOfLines.indexOf(line) - 1, y: i - 1 },
+                    { x: arrayOfLines.indexOf(line) - 1, y: i },
+                    { x: arrayOfLines.indexOf(line) - 1, y: i + 1 },
+                    { x: arrayOfLines.indexOf(line), y: i - 1 },
+                    { x: arrayOfLines.indexOf(line), y: i },
+                    { x: arrayOfLines.indexOf(line), y: i + 1 },
+                    { x: arrayOfLines.indexOf(line) + 1, y: i - 1 },
+                    { x: arrayOfLines.indexOf(line) + 1, y: i },
+                    { x: arrayOfLines.indexOf(line) + 1, y: i + 1 })
+            }
+            if (isNaN(line[i]) || i === numberOfColumns - 1 || !line[i]) {
+                if (number.value.length > 0) {
+                    number.value = Number(number.value.trim())
+                    lineData.numbers.push(number);
+                    number = {
+                        value: '',
+                        validationCoordinates: []
+                    };
+                }
+            }
+        }
+        linesData.push(lineData)
+    }
+    return linesData
+}
+
+const validateCaracter = function (x, y) {
+
+    x = Number(x);
+    y = Number(y);
+
+    if (x >= 0 && y >= 0 && x < arrayOfLines.length && y < numberOfColumns) {
+        let caracter = arrayOfLines[x][y];
+        if (caracter !== '.' && isNaN(caracter)) {
+            return true;
+        }
+    }
+
+    return false
+};
+
+const validateNumbers = function () {
+    let result = 0
+    const allLinesNumbers = getAllNumbers()
+    for (let i = 0; i < allLinesNumbers.length; i++) {
+        const line = allLinesNumbers[i];
+        for (const number of line.numbers) {
+            for (const coordinates of number.validationCoordinates) {
+                if (validateCaracter(coordinates.x, coordinates.y)) {
+                    console.log(`From line num. ${line.id}, the number ${number.value} is flagged !`)
+                    result = result + number.value
+                    break
+                }
+            }
+        }
+    }
+    return result
+}
+
+console.log(validateNumbers())
---
+++ day03/app_02.js
@@ -0,0 +1,143 @@
+// Import the Node.js file system
+const fs = require('fs');
+
+// Use the file system to read the text file and encode it in UTF-8
+const fileContent = fs.readFileSync('input.txt', 'utf8');
+
+// Create an array using the array.split() method
+// Split is performed using the carriage return as a separator
+const arrayOfLines = fileContent.split('\n');
+const numberOfRows = Number(arrayOfLines.length)
+const numberOfColumns = Number(arrayOfLines[0].trim().length)
+console.log(`Rows:`, numberOfRows, `Columns:`, numberOfColumns)
+
+// Parse the file to retrieve all the stars by row
+const allStarsByLine = function () {
+    let linesData = [];
+    for (const line of arrayOfLines) {
+        let lineData = {
+            lineNumber: Number(arrayOfLines.indexOf(line) + 1),
+            starCollection: []
+        };
+        let starData = {
+            index: '',
+            validationCoordinates: []
+        };
+        for (let i = 0; i < numberOfColumns; i++) {
+            if (line[i] === '*') {
+                starData.index = Number(i)
+                starData.validationCoordinates.push(
+                    { row: arrayOfLines.indexOf(line) - 1, column: i - 1 },
+                    { row: arrayOfLines.indexOf(line) - 1, column: i },
+                    { row: arrayOfLines.indexOf(line) - 1, column: i + 1 },
+                    { row: arrayOfLines.indexOf(line), column: i - 1 },
+                    // { row: arrayOfLines.indexOf(line), column: i },
+                    { row: arrayOfLines.indexOf(line), column: i + 1 },
+                    { row: arrayOfLines.indexOf(line) + 1, column: i - 1 },
+                    { row: arrayOfLines.indexOf(line) + 1, column: i },
+                    { row: arrayOfLines.indexOf(line) + 1, column: i + 1 })
+                lineData.starCollection.push(starData)
+                starData = {
+                    index: '',
+                    validationCoordinates: []
+                };
+            }
+        }
+        linesData.push(lineData)
+
+    }
+    return linesData
+}
+
+const checkGear = function (validationCoordinates) {
+    let checker = 0
+    for (let coordinates of validationCoordinates) {
+        if (coordinates.row >= 0 && coordinates.column >= 0 && coordinates.row < arrayOfLines.length && coordinates.column < numberOfColumns) {
+            let caracter = arrayOfLines[coordinates.row][coordinates.column];
+            if (!isNaN(caracter)) {
+                checker++;
+            }
+        }
+    }
+    return checker > 1 ? true : false
+}
+
+const getValidStars = function () {
+    let validatedStars = []
+    for (let line of allStarsByLine()) {
+        for (star of line.starCollection) {
+            if (checkGear(star.validationCoordinates)) {
+                validatedStars.push({
+                    row: line.lineNumber,
+                    column: star.index
+                })
+            }
+        }
+    }
+    return validatedStars
+}
+
+
+const scanNumbersAround = function (starsCoordinates) {
+    const valuesToAdd = [];
+
+    for (const starCoordinates of starsCoordinates) {
+        const row = starCoordinates.row;
+        const column = starCoordinates.column;
+        const allAdjacentValues = []
+        // Extract adjacent values using array slicing
+        if (arrayOfLines[row - 1]) {
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 3]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 2]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 1]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 2]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 1]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 0]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 1]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 0]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column + 1]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row - 1][starCoordinates.column - 0]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column + 1]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column + 2]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row - 1][starCoordinates.column + 1]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column + 2]}${arrayOfLines[starCoordinates.row - 1][starCoordinates.column + 3]}`);
+        }
+
+        allAdjacentValues.push(`${arrayOfLines[starCoordinates.row + 0][starCoordinates.column - 3]}${arrayOfLines[starCoordinates.row + 0][starCoordinates.column - 2]}${arrayOfLines[starCoordinates.row + 0][starCoordinates.column - 1]}`);
+        allAdjacentValues.push(`${arrayOfLines[starCoordinates.row + 0][starCoordinates.column + 1]}${arrayOfLines[starCoordinates.row + 0][starCoordinates.column + 2]}${arrayOfLines[starCoordinates.row + 0][starCoordinates.column + 3]}`);
+
+        if (arrayOfLines[row + 1]) {
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 3]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 2]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 1]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 2]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 1]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 0]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 1]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 0]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column + 1]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row + 1][starCoordinates.column - 0]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column + 1]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column + 2]}`);
+            allAdjacentValues.push(`${arrayOfLines[starCoordinates.row + 1][starCoordinates.column + 1]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column + 2]}${arrayOfLines[starCoordinates.row + 1][starCoordinates.column + 3]}`);
+        }
+        console.log(allAdjacentValues)
+        // Filter non-whole numbers using regular expressions
+        const filteredAdjacentValues = allAdjacentValues.filter(value => {
+            const regex = new RegExp(/^\d+$/);
+            return regex.test(value);
+        });
+
+        let product; // Declare variable to store product
+
+        if (filteredAdjacentValues.length > 1) {
+            product = filteredAdjacentValues.reduce((acc, value) => acc * value, 1);
+        } else if (filteredAdjacentValues.length === 1) {
+            product = filteredAdjacentValues[0];
+        } else {
+            product = 0;
+        }
+
+        valuesToAdd.push(product)
+
+    }
+
+    return valuesToAdd;
+};
+
+
+// let test = allStarsByLine()
+// console.log(test[1])
+// console.log(checkGear(test[1].starCollection[0].validationCoordinates))
+console.log(getValidStars())
+let test = getValidStars()
+let test2 = scanNumbersAround(test)
+console.log(test2)
+const solution = test2.reduce(
+    (accumulator, current) => Number(accumulator) + Number(current), 0
+)
+console.log(solution)
+
